1f92911b1fbc02805dea31b55614d61a81bde2fb
Hauke Wendt <hauke.wendt@stud-mail.uni-wuerzburg.de>
2022-01-13 15:22:28 +0100

    reactApp update1

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Button from "@mui/material/Button";
+
+const App = () => {
+    return (
+        // <div>
+        //     <h1>Website incoming...</h1>
+        // </div>
+
+        <Button variant="contained" color="primary"> Website incoming</Button>
+    );
+}
+
+export default App;
